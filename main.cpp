5947427b7312de89006ca95f40cf7fde76737a16
danermeister <70635084+danermeister@users.noreply.github.com>
2020-09-30 13:12:07 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -92,7 +92,25 @@ int main()
 		l1++;
 	}
 	cout<<endl;
-//	Can you can a can as a canner can can a can?
- 
-	return 0;
-}
+   	std::cout << "vvedi zameny" << std::endl;
+       	std::string str2 = "Can you can a can as a canner can can a can";
+       	std::string str3;
+       	std::getline(std::cin, str3);
+       	std::string strc = "can";
+       		while (str2.find(strc) != std::string::npos) 
+		{
+         		 auto pos = str2.find(strc);
+           		 str2.erase(pos, strc.size());
+          		 str2.insert(pos, str3);
+       		}
+       strc = "Can";
+       if ((str3[0] >= 97) && (str3[0] <= 122)) str3[0] -= 32; 
+       while (str2.find(strc) != std::string::npos) 
+       {
+           auto pos = str2.find(strc);
+           str2.erase(pos, strc.size());
+           str2.insert(pos, str3);
+       }
+       std::cout << str2<<endl;
+       return 0;
+   }
